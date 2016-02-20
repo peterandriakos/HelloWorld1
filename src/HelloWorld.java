4a465777ad2ad8dae9da54dece264aa30a45f26d
peterandriakos <peter.andriakos@maine.edu>
2016-02-20 18:56:53 -0500

    Display Free Memory Feature Added
    closes #4

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println
+			("Hello World!");
+		/* Issue1: Display total amount of free memory available to the JVM/User */
+		 System.out.println
+		 	("Free memory (bytes): " + Runtime.getRuntime().freeMemory());
 
 	}
 
