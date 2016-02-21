0e3600b954837bd19e7424494ce3cd74db1ef147
peterandriakos <peter.andriakos@maine.edu>
2016-02-21 00:21:12 -0500

    Display Current Date Feature Added
    closes #3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
-
+		/* Issue2: Display the current date to the user */
+		System.out.print
+			(java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME.format
+				(java.time.ZonedDateTime.now()).replaceFirst(".....$", "GMT") + ": "); 	// replace UTC offset with GMT
 	}
 
 }
